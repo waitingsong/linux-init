5cad61c40561967f1f1c396a8fd633841fea7349
waiting <1661926154@qq.com>
2025-02-10 17:03:53 +0800

    chore: update scripts

--- src/rocky/config/profile.d/z.alias.zfs.sh
+++ src/rocky/config/profile.d/z.alias.zfs.sh
@@ -2,10 +2,10 @@
 # cp -u z.alias.zfs.sh /etc/profile.d/
 
 
-export zlist_opts='name,used,available,referenced,mountpoint,compression,compressratio,recordsize,logbias,sync,primarycache,readonly,mounted,creation,'
+export zlist_opts='name,used,available,referenced,mountpoint,compression,compressratio,recordsize,logbias,sync,primarycache,mounted,creation,'
 alias zlist="zfs list -o "${zlist_opts}""
 
-export zlist_full_opts='name,used,available,referenced,logicalreferenced,logicalused,mountpoint,mounted,compressratio,refcompressratio,type,encryption,copies,dedup,xattr,quota,volblocksize,version,written,guid,'
+export zlist_full_opts='name,used,available,referenced,logicalreferenced,logicalused,mounted,compressratio,refcompressratio,type,encryption,copies,dedup,xattr,quota,volblocksize,version,written,readonly,'
 alias zlist_full="zfs list -o "${zlist_full_opts}""
 
 export zpool_full_opts='name,size,allocated,free,freeing,checkpoint,expandsize,fragmentation,capacity,dedupratio,health,altroot,fragmentation,readonly,ashift,autoexpand,autoreplace,autotrim,cachefile,version,'

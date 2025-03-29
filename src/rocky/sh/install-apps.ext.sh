d7c589e4e475b9dc5dafb67e2e46fbb8c2335488
waiting <1661926154@qq.com>
2025-03-29 20:25:14 +0800

    update pigsty

--- src/rocky/sh/install-apps.ext.sh
+++ src/rocky/sh/install-apps.ext.sh
@@ -4,9 +4,11 @@ set -euo pipefail
 
 echo ==================== 安装基本编译环境 ====================
 dnf groupinstall -y "Development Tools"
-
 dnf install -y \
-  cmake
+  cmake \
+  clang clang-devel \
+  glibc glibc-devel \
+  gcc gcc-c++ gcc-toolset-13-gcc  gcc-toolset-13-gcc-c++ \
 
 
 echo ==================== 安装安装集群相关软件 ====================
